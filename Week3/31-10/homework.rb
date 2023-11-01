b9b29dd316613be64a96d1809c6a3083a5e90d15
khoadev2512 <khoa.nguyencse.npng@hcmut.edu.vn>
2023-11-01 09:53:17 +0700

    31/10 Homework

--- Week3/31-10/homework.rb
+++ Week3/31-10/homework.rb
@@ -1,6 +1,9 @@
-def convertDNA(dna)
-  arr = {"A" => "T", "T" => "A", "C" => "G", "G" => "C"}
-  dna = dna.split("")
-  return "Wrong DNA" if (dna == [] || dna.any? { |i| !arr.keys.include?(i) })
-  dna.map { |i| i = arr[i] }.join
+# frozen_string_literal: true
+
+def convert_dna(dna)
+  arr = { 'A' => 'T', 'T' => 'A', 'C' => 'G', 'G' => 'C' }
+  dna = dna.split('')
+  return 'Wrong DNA' if dna == [] || dna.any? { |i| !arr.keys.include?(i) }
+
+  dna.map { |i| arr[i] }.join
 end
