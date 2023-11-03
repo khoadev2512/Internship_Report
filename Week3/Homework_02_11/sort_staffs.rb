324ae9e928abe6de8cd140e9090c0e8079b639c3
khoadev2512 <khoa.nguyencse.npng@hcmut.edu.vn>
2023-11-03 09:53:56 +0700

    02/11 homework

--- Week3/Homework_02_11/sort_staffs.rb
+++ Week3/Homework_02_11/sort_staffs.rb
@@ -3,7 +3,8 @@
 def sort_staffs(staffs)
   positions = { 'tonggiamdoc' => 0, 'giamdoc' => 1, 'truongphong' => 2, 'nhanvienvp' => 3, 'nhanviensale' => 4 }
   staffs.sort_by do |staff|
-    max_priority = staff[:positions].min_by { |position| positions[position] }
+    max_priority = staff[:positions].min_by { |pos| positions[pos] }
+    staff[:positions].sort_by! { |position| positions[position] }
     [positions[max_priority], staff[:id]]
   end
 end
