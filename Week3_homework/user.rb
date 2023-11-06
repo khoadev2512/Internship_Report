dbc48cbab6994f3e6a62780bd38d26d7c78684ff
khoadev2512 <khoa.nguyencse.npng@hcmut.edu.vn>
2023-11-06 09:43:16 +0700

    03/11 Homework

--- Week3_homework/user.rb
+++ Week3_homework/user.rb
@@ -17,28 +17,29 @@ def get_users_by_active(conn, act)
   JSON.parse(conn.get('/api/v1/users', { active: act }).body)
 end
 
-def add_user(conn)
+def add_user(conn, user)
   existing_data = get_users(conn)
   # Remove the last element if the array size exceeds the maximum
   conn.delete("/api/v1/users/#{existing_data.last['id'].to_i}") if existing_data.count == 100
-  data = {
-    'created_at' => Time.now.to_s,
-    'name' => 'Nguyen Van Khoa',
-    'avatar' => 'https://duhocvietglobal.com/wp-content/uploads/2018/12/dat-nuoc-va-con-nguoi-anh-quoc.jpg',
-    'sex' => 'male',
-    'active' => true,
-    'id' => existing_data.last['id']
-  }
-  json_data = JSON.generate(data)
 
   # Send the updated JSON array in the request body
   conn.post do |req|
     req.url '/api/v1/users'
     req.headers['Content-Type'] = 'application/json'
-    req.body = json_data
+    req.body = user
   end
 end
 
 p get_users(conn)
 p get_users_by_active(conn)
-p add_user(conn)
+
+data = {
+  'created_at' => Time.now.to_s,
+  'name' => 'Nguyen Van Khoa',
+  'avatar' => 'https://duhocvietglobal.com/wp-content/uploads/2018/12/dat-nuoc-va-con-nguoi-anh-quoc.jpg',
+  'sex' => 'male',
+  'active' => true,
+  'id' => existing_data.last['id']
+}
+json_data = JSON.generate(data)
+p add_user(conn, json_data)
